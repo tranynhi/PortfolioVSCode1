7fd69f5307e6bef8f791b0d4a2b93303d25d7f5a
Tran Y Nhi <tranynhi@gmail.com>
2025-04-16 12:10:05 +0700

    Add health check endpoint for UptimeRobot

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
+// Health check endpoint for UptimeRobot
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 // Serve static files
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
